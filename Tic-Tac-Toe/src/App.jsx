77060a6aab07dbdeb4f757dbbd68c1435a129624
Luis-Pazos-JS <luispazos5533@gmail.com>
2026-04-29 23:57:35 -0300

    prueba tecnica

--- Tic-Tac-Toe/src/App.jsx
+++ Tic-Tac-Toe/src/App.jsx
@@ -1,49 +1,46 @@
 import { useState } from "react";
-import "./App.css";
 import confetti from "canvas-confetti";
 import { Square } from "./components/Square.jsx";
-import { WINNER,TURN } from "./data/Const.js";
+import { WINNER, TURN } from "./data/Const.js";
 import { checkEndGame, checkwinner } from "./models/Logic.js";
 import { WinnerModal } from "./components/WinnerModal.jsx";
 
-
 function App() {
   const [board, setBoard] = useState(() => {
-    const loadBoardStorage = window.localStorage.getItem('board')
+    const loadBoardStorage = window.localStorage.getItem("board");
     console.log(loadBoardStorage);
-    
+
     return loadBoardStorage
       ? JSON.parse(loadBoardStorage)
-      : Array(9).fill(null) 
+      : Array(9).fill(null);
   });
 
   const [winner, setWinner] = useState(null);
 
   const resetGame = () => {
-    setBoard(Array(9).fill(null))
-    setTurn(TURN.X)
-    setWinner(null)
-  }
+    setBoard(Array(9).fill(null));
+    setTurn(TURN.X);
+    setWinner(null);
+  };
   const updateBoard = (index) => {
     if (board[index] || winner) return;
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
-    window.localStorage.setItem('board', JSON.stringify(newBoard));
-    window.localStorage.setItem('turn', turn);
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", turn);
     console.log(turn);
-    
+
     const newWinner = checkwinner(newBoard);
     if (newWinner) {
-      confetti()
+      confetti();
       setWinner(newWinner);
-
     } else if (checkEndGame(newBoard)) {
-      setWinner(WINNER.TIE)
+      setWinner(WINNER.TIE);
     }
     const newTurn = turn === TURN.X ? TURN.O : TURN.X;
     setTurn(newTurn);
-  }
+  };
   const [turn, setTurn] = useState(TURN.X);
   return (
     <main className="board">
